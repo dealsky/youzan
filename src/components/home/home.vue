ed3c177ec6df0a73115565408afa78ff054e98b3
dealsky <dealsky000@outlook.com>
2018-04-09 20:48:06 +0800

    home Header

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,69 @@
 <template>
-  <div>
-    i am home
+  <div class="home">
+    <div class="header">
+      <ul class="item-list">
+        <li class="item">
+          <i class="icon icon-receipt"></i>
+          <span class="text">收款</span>
+        </li>
+        <li class="item">
+          <i class="icon icon-shopping_cart"></i>
+          <span class="text">开单</span>
+        </li>
+        <li class="item">
+          <i class="icon icon-scan"></i>
+          <span class="text">扫一扫</span>
+        </li>
+      </ul>
+    </div>
+    <div class="content">
+      <ul class="item-list">
+        <li class="item">
+          <i class="icon"></i>
+          <span class="text">商品管理</span>
+        </li>
+        <li class="item">
+          <i class="icon"></i>
+          <span class="text">我的收入</span>
+        </li>
+        <li class="item">
+          <i class="icon"></i>
+          <span class="text">消息</span>
+        </li>
+        <li class="item">
+          <i class="icon"></i>
+          <span class="text">台卡</span>
+        </li>
+        <li class="item">
+          <i class="icon"></i>
+          <span class="text">顾客</span>
+        </li>
+        <li class="item">
+          <i class="icon"></i>
+          <span class="text">会员充值</span>
+        </li>
+        <li class="item">
+          <i class="icon"></i>
+          <span class="text">优惠券</span>
+        </li>
+        <li class="item">
+          <i class="icon"></i>
+          <span class="text">会员卡</span>
+        </li>
+        <li class="item">
+          <i class="icon"></i>
+          <span class="text">活动</span>
+        </li>
+        <li class="item">
+          <i class="icon"></i>
+          <span class="text">储值</span>
+        </li>
+        <li class="item">
+          <i class="icon"></i>
+          <span class="text">会员通知</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -9,5 +72,30 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
 
+  .home
+    .header
+      width: 100%
+      height: 190px
+      background: $color-background-top
+      .item-list
+        display: flex
+        align-items: center
+        height: 100%
+        .item
+          display: inline-block
+          flex: 1
+          text-align: center
+          color: $color-text-y
+          .icon
+            display: block
+            margin-bottom: 12px
+            font-size: 50px
+          .text
+            font-size: $font-size-small-l
+    .content
+      .item-list
+        .item
+          display: inline-block
 </style>
